8a4cba305a114b4a407553972a4f9349b7fa3d3b
DiegoPuin <39832209+DiegoPuin@users.noreply.github.com>
2020-03-31 16:25:16 -0500

    Add files via upload

---
+++ Compilador/src/Analizador/Tokens.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizador;
+
+
+/**
+ *
+ * @author Andres Ramirez
+ * Codigo: 20161020077
+ * Ciencias de la computacion 3
+ */
+public enum Tokens {
+    Linea,
+    Comillas,
+    Comillas_S,
+    T_datoN,
+    Cadena,
+    Vacio,
+    Nulo,
+    Include,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Switch,
+    Case,
+    Break,
+    Default,
+    Print,
+    Scan,
+    Gets,
+    Return,
+    Unsigned,
+    Sistema,
+    SizeOf,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Modulo,
+    Op_logico,
+    Op_relacional,
+    Op_atribucion,
+    Op_incremento,
+    Op_booleano,
+    Numeral,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    P_coma,
+    Dos_P,
+    Identificador,
+    Numero,
+    ERROR
+}
